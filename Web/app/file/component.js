d657c925d010592e3d506689fef364ffad68aa18
Nabeel Tanveer <Nabeel.Tanveer@EYC.COM>
2016-05-09 22:43:57 +0100

    csv app upload updated.

--- Web/app/file/component.js
+++ Web/app/file/component.js
@@ -1,22 +1,25 @@
 var csvApp = angular.module('csvApp');
-csvApp.controller('csvFileCtrl', function ($http) {
-    this.uploadFile = function () {
 
-        console.log("button clicked.");
-
-        var file = $('#js-import').find('input')[0].files[0],
-                    formData = new FormData();
-            formData.append('file0', file);
-
-            $http
-                .post('http://localhost:8421/file/upload', formData, {})
-                .then(function(response) {});
-    };
-})
-.directive('csvFile', [function() {
+csvApp.directive('csvFile', function($http) {
     return {
         restrict: 'E',
+        scope: { },
         templateUrl: 'app/file/component.html',
-        controller: 'csvFileCtrl'
+        link: function(scope, element, attrs) {
+            document.getElementById('js-file').onchange = function () {
+
+                console.log("uploading...");
+
+                var file = $('#js-import').find('input')[0].files[0],
+                            formData = new FormData();
+                formData.append('file0', file);
+
+                $http
+                    .post('http://localhost:8421/file/upload', formData, {
+                        transformRequest: angular.identity,
+                        headers: { 'Content-Type': undefined }
+                    }).then(function (response) { });
+            };
+        }
     };
-}]);
+});
